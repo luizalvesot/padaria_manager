cebe312c58e86250afca66f705611ca78202706a
luizalvesot <alvesluizotavio23@gmail.com>
2024-08-20 23:48:02 -0300

    tela para mostrar clientes iniciada

---
+++ resources/views/livewire/manager/clientes/index.blade.php
@@ -0,0 +1,69 @@
+<div>
+    <!-- linha dos botões -->
+    <div class="row pt-2 pb-2 text-center">
+        <div class="col">
+            <button class="btn btn-sm btn-dark px-3" type="button"><i class="bi bi-plus-lg"></i></button>
+        </div>
+    </div>
+
+    <!-- formulario -->
+    <form class="row pt-2 pb-3 my-2 bg-white shadow rounded">
+        <div class="col-md-3">
+            <label class="text-dark"><strong>Nome</strong></label>
+            <input type="text" placeholder="Nome" 
+                    class="form-control border rounded py-1 px-2 shadow-sm">
+        </div>
+        <div class="col-md-3">
+            <label class="text-dark"><strong>Cidade</strong></label>
+            <input type="text" placeholder="Cidade" 
+                    class="form-control border rounded py-1 px-2 shadow-sm">
+        </div>
+        <div class="col-md-2">
+            <label class="text-dark"><strong>Telefone</strong></label>
+            <input type="text" placeholder="Telefone" 
+                    class="form-control border rounded py-1 px-2 shadow-sm">
+        </div>
+        <div class="col-md-2">
+            <label class="text-dark"><strong>Status</strong></label>
+            <select class="form-select border rounded py-1 px-3 shadow-sm">
+                <option value="">Todos</option>
+                <option value=1>Ativo</option>
+                <option value=0>Inativo</option>
+            </select>
+        </div>
+        <div class="col-md-2">
+            <button class="btn btn-primary btn-sm px-5 mt-4">Pesquisar</button>
+        </div>
+    </form>
+
+    <!-- tabela de clientes -->
+    <div class="row table-responsive p-2 bg-white shadow rounded">
+        <table class="table table-hover table-striped">
+            <caption>Lista de clientes</caption>
+            <thead>
+                <tr>
+                    <th scope="col">Código</th>
+                    <th scope="col">Nome/Razão social</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">CPF/CNPJ</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($clientes as $cliente)
+                    <tr>
+                        <td>{{ $cliente->id }}</td>
+                        <td>{{ $cliente->nome_cliente }}</td>
+                        <td>{{ $cliente->tipo_cliente }}</td>
+                        <td>{{ $cliente->cpf_cliente }}</td>
+                        <td>{{ $cliente->telefone_celular_cliente }}</td>
+                        <td>{{ $cliente->status_cliente }}</td>
+                        <td> - </td>
+                    </tr> 
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
--- resources/views/manager/clientes/index.blade.php
+++ resources/views/manager/clientes/index.blade.php
@@ -1,3 +1,13 @@
 <x-app-layout>
-    
+    <div class="container">
+        <div class="row pt-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Clientes</li>
+                </ol>
+            </nav>
+        </div><hr>
+        <livewire:manager.clientes/>
+    </div>
 </x-app-layout>
