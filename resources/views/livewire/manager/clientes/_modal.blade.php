0ba439159c25a7ecb17000b21e2ed095df2ceb16
luizalvesot <alvesluizotavio23@gmail.com>
2024-08-25 11:47:46 -0300

    modal incluido para mostrar informacoes do cliente

---
+++ resources/views/livewire/manager/clientes/_modal.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <div class="modal fade modal-xl" id="clienteModal" tabindex="-1" aria-labelledby="clienteModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="clienteModalLabel">Detalhes do Cliente</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- Conteúdo carregado via AJAX -->
+                    <div id="clienteDetalhes">
+                        Carregando...
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
