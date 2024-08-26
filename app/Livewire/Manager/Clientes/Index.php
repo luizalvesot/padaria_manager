7dc1fc3d158a58a7a5efb9306b34673321f6574f
luizalvesot <alvesluizotavio23@gmail.com>
2024-08-25 23:03:13 -0300

    alterado o valor de paginate para 10

--- app/Livewire/Manager/Clientes/Index.php
+++ app/Livewire/Manager/Clientes/Index.php
@@ -13,7 +13,7 @@ class Index extends Component
     public function render()
     {
         $clientes = Cliente::where('nome_cliente', '!=', null)
-                    ->paginate(1);
+                    ->paginate(10);
 
         return view('livewire.manager.clientes.index', compact('clientes'));
     }
