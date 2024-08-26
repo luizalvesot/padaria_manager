aa2368f1173bd6ec005baa80e0726989c6e1d570
luizalvesot <alvesluizotavio23@gmail.com>
2024-08-26 19:38:33 -0300

    feito importacao do jquery mask

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
 //import './bootstrap';
 
 import 'bootstrap';
+import $ from 'jquery';
+import 'jquery-mask-plugin';
+
+$(document).ready(function() {
+    $('.phone').mask('(00) 00000-0000');
+});
