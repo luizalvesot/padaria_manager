0f05298954db43e1b88e612c3a0990dc0169f008
luizalvesot <alvesluizotavio23@gmail.com>
2024-08-27 12:26:51 -0300

    criado metodo para atualizar dados do cliente

--- app/Http/Controllers/Manager/Clientes/ClientesController.php
+++ app/Http/Controllers/Manager/Clientes/ClientesController.php
@@ -93,16 +93,80 @@ class ClientesController extends Controller
         );
     }
 
+    /**
+     * Route: clientes/{cliente}/edit/
+     * Name: clients.edit
+     * Method: GET
+     **/
+    public function edit(Cliente $cliente)
+    {
+        return view('manager.clientes.edit', compact('cliente'));
+    }
+
+    /**
+     * Route: clientes/{cliente}/
+     * Name: clientes.update
+     * Method: PUT
+     **/
+    public function update(Request $request, Cliente $cliente)
+    {
+        $request->validate([
+            'nome_cliente' => 'required|max:255',
+            'nascimento_cliente' => 'required',
+            'tipo_cliente' => 'required',
+            'cpf_cliente' => 'nullable',
+            'rg_cliente' => 'nullable',
+            'cnpj_cliente' => 'nullable',
+            'nome_fantasia_cliente' => 'nullable',
+            'status_cliente' => 'required',
+            'telefone_celular_cliente' => 'required',
+            'telefone_fixo_cliente' => 'nullable',
+            'email_cliente' => 'nullable',
+            'cep_cliente' => 'nullable',
+            'cidade_cliente' => 'nullable',
+            'estado_cliente' => 'nullable',
+            'bairro_cliente' => 'nullable',
+            'logradouro_cliente' => 'nullable',
+            'referencia_cliente' => 'nullable',
+            'numero_cliente' => 'nullable',
+            'observacoes' => 'nullable',
+        ]);
+
+        $cliente->update([
+            'nome_cliente'              => $request->nome_cliente,
+            'nascimento_cliente'        => $request->nascimento_cliente,
+            'tipo_cliente'              => $request->tipo_cliente,
+            'cpf_cliente'               => $request->cpf_cliente,
+            'rg_cliente'                => $request->rg_cliente,
+            'cnpj_cliente'              => $request->cnpj_cliente,
+            'nome_fantasia_cliente'     => $request->nome_fantasia_cliente,
+            'status_cliente'            => $request->status_cliente,
+            'telefone_celular_cliente'  => $request->telefone_celular_cliente,
+            'telefone_fixo_cliente'     => $request->telefone_fixo_cliente,
+            'email_cliente'             => $request->email_cliente,
+            'cep_cliente'               => $request->cep_cliente,
+            'cidade_cliente'            => $request->cidade_cliente,
+            'estado_cliente'            => $request->estado_cliente,
+            'bairro_cliente'            => $request->bairro_cliente,
+            'logradouro_cliente'        => $request->logradouro_cliente,
+            'referencia_cliente'        => $request->referencia_cliente,
+            'numero_cliente'            => $request->numero_cliente,
+            'observacoes'               => $request->observacoes,
+        ]);
+
+        return Swal::redirect(
+            'info',
+            'Alteração de Cliente!',
+            'As informações do cliente foram atualizadas com sucesso em nosso sistema!',
+            'clients.show'
+        );
+    }
+
     public function showModal($id)
     {
         $cliente = Cliente::findOrFail($id);
 
         return view('livewire.manager.clientes.detalhes', compact('cliente'));
-    }
-
-    public function edit(Cliente $cliente)
-    {
-        return view('manager.clientes.edit', compact('cliente'));
     }
 
     /**
