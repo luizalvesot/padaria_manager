0a20107c24deacafccf6881f68a3d281682dfa23
luizalvesot <alvesluizotavio23@gmail.com>
2024-08-27 15:21:57 -0300

    pesquisa de clientes implementada

--- app/Livewire/Manager/Clientes/Index.php
+++ app/Livewire/Manager/Clientes/Index.php
@@ -10,11 +10,29 @@ class Index extends Component
 {
     use WithPagination;
 
+    public $nome_cliente = '';
+    public $cidade_cliente = '';
+    public $telefone_celular_cliente = '';
+    public $status_cliente = '';
+
+    public function search_cliente()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        $clientes = Cliente::where('nome_cliente', '!=', null)
+        $clientes = Cliente::where('nome_cliente', 'like', "%".$this->nome_cliente."%")
+                    ->where('cidade_cliente', 'like', "%".$this->cidade_cliente."%")
+                    ->where('telefone_celular_cliente', 'like', "%".$this->telefone_celular_cliente."%")
+                    ->where('status_cliente', 'like', "%".$this->status_cliente."%")
                     ->paginate(10);
 
         return view('livewire.manager.clientes.index', compact('clientes'));
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
     }
 }
