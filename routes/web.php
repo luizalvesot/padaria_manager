ee797df0cd5f3d43e4f1346b2965bfefe6a13d04
luizalvesot <alvesluizotavio23@gmail.com>
2024-09-04 19:28:25 -0300

    rotas ajustadas

--- routes/web.php
+++ routes/web.php
@@ -19,70 +19,64 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
+/**
+ * 
+ * Grupo de rotas utilizadas nas views de FORNECEDORES
+ * 
+ */
 Route::prefix('/fornecedores')->group(function(){
-    Route::get('/', 
-        [FornecedoresController::class, 'show'])
-    ->name('fornecedores.show');
+    Route::get('/', [FornecedoresController::class, 'show'])->name('fornecedores.show');
 
-    Route::get('/create', 
-        [FornecedoresController::class, 'create'])
-    ->name('fornecedores.create');
+    Route::get('/create', [FornecedoresController::class, 'create'])->name('fornecedores.create');
 
-    Route::post('/store', 
-        [FornecedoresController::class, 'store'])
-    ->name('fornecedores.store');
+    Route::post('/store', [FornecedoresController::class, 'store'])->name('fornecedores.store');
 
-    Route::get('{fornecedor}/edit', 
-        [FornecedoresController::class, 'edit'])
-    ->name('fornecedores.edit');
+    Route::get('{fornecedor}/edit', [FornecedoresController::class, 'edit'])->name('fornecedores.edit');
 
-    Route::put('{fornecedor}', 
-        [FornecedoresController::class, 'update'])
-    ->name('fornecedores.update');
+    Route::put('{fornecedor}', [FornecedoresController::class, 'update'])->name('fornecedores.update');
 
-    Route::delete('/{fornecedor}', 
-        [FornecedoresController::class, 'destroy'])
-    ->name('fornecedores.destroy');
+    Route::delete('/{fornecedor}', [FornecedoresController::class, 'destroy'])->name('fornecedores.destroy');
 
-    Route::get('/{id}', 
-        [FornecedoresController::class, 'showModal'])
-    ->name('fornecedores.showModal');
+    Route::get('/{id}', [FornecedoresController::class, 'showModal'])->name('fornecedores.showModal');
 });
 
+/**
+ * 
+ * Grupo de rotas utilizadas nas views de CLIENTES
+ */
 Route::prefix('/clientes')->group(function(){
-    Route::get('/', 
-        [ClientesController::class, 'show'])
-    ->name('clientes.show');
-
-    Route::get('/create', 
-        [ClientesController::class, 'create'])
-    ->name('clientes.create');
-
-    Route::post('/store', 
-        [ClientesController::class, 'store'])
-    ->name('clientes.store');
-
-    Route::get('{cliente}/edit', 
-        [ClientesController::class, 'edit'])
-    ->name('clientes.edit');
-
-    Route::put('{cliente}', 
-        [ClientesController::class, 'update'])
-    ->name('clientes.update');
-
-    Route::delete('/{cliente}', 
-        [ClientesController::class, 'destroy'])
-    ->name('clientes.destroy');
-
-    Route::get('/{id}', 
-        [ClientesController::class, 'showModal'])
-    ->name('clientes.showModal');
+    Route::get('/', [ClientesController::class, 'show'])->name('clientes.show');
+    
+    Route::get('/create', [ClientesController::class, 'create'])->name('clientes.create');
+    
+    Route::post('/store', [ClientesController::class, 'store'])->name('clientes.store');
+    
+    Route::get('{cliente}/edit', [ClientesController::class, 'edit'])->name('clientes.edit');
+    
+    Route::put('{cliente}', [ClientesController::class, 'update'])->name('clientes.update');
+    
+    Route::delete('/{cliente}', [ClientesController::class, 'destroy'])->name('clientes.destroy');
+    
+    Route::get('/{id}', [ClientesController::class, 'showModal'])->name('clientes.showModal');
 });
 
-Route::get('pdf',
-    [ClientesController::class, 'geraPdf'])
-->name('clientes.pdf');
+/**
+ * 
+ * Grupo de rotas utilizadas para tratar PDF
+ * 
+ */
+Route::prefix('/pdf')->group(function(){
+    Route::get('clientes', [ClientesController::class, 'geraPdf'])->name('clientes.pdf');
 
+    Route::get('fornecedores', [FornecedoresController::class, 'geraPdf'])->name('fornecedores.pdf');
+});
+
+
+/**
+ * 
+ * Rotas do Jetstream
+ * 
+ */
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
