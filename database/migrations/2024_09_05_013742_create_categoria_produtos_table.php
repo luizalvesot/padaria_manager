e379392a65db7afb086d1c0fbc76675cd07bd3fb
luizalvesot <alvesluizotavio23@gmail.com>
2024-09-04 22:42:59 -0300

    criado tabela de categorias de produtos

---
+++ database/migrations/2024_09_05_013742_create_categoria_produtos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categoria_produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('chave_categoria', 80);
+            $table->string('nome_categoria', 80);
+            $table->string('descricao_categoria', 100)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categoria_produtos');
+    }
+};
