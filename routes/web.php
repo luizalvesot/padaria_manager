749232dfa644755efa4c3f83964666f0a5e820d6
luizalvesot <alvesluizotavio23@gmail.com>
2024-09-09 21:15:23 -0300

    rotas corrigidas

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Manager\Clientes\ClientesController;
 use App\Http\Controllers\Manager\Fornecedores\FornecedoresController;
 use App\Http\Controllers\Manager\Produtos\CategoriaProdutos\CategoriasController;
-use App\Http\Controllers\Manager\Produtos\TipoMedidas\MedidasController;
+use App\Http\Controllers\Manager\Produtos\TiposMedidas\MedidasController;
 
 /*
 |--------------------------------------------------------------------------
