32a5a14cff5bf5499b32a5c3319562be25823895
luizalvesot <alvesluizotavio23@gmail.com>
2024-09-17 22:59:23 -0300

    implementada pagina de listagem de produtos

--- resources/views/livewire/manager/produtos/index.blade.php
+++ resources/views/livewire/manager/produtos/index.blade.php
@@ -1,49 +1,60 @@
 <div>
     <!-- formulario -->
-    <form class="row pt-2 pb-3 my-2 bg-white shadow rounded">
-        <div class="col-md-3">
-            <label class="text-dark"><strong>Descrição</strong></label>
-            <input type="text" placeholder="Descrição" wire:model.defer="descricao_produto" id="descricao_produto"
-                    class="form-control border rounded py-1 px-2 shadow-sm">
+    <form class="row pt-3 px-2 pb-3 my-2 bg-light shadow-sm rounded border">
+        <div class="row mb-2">
+            <div class="col-md-2">
+                <label class="text-dark"><strong>Códido</strong></label>
+                <input type="text" wire:model.defer="id_produto" id="id"
+                        class="form-control border rounded py-1 px-2">
+            </div>
+            <div class="col-md-5">
+                <label class="text-dark"><strong>Descrição</strong></label>
+                <input type="text" wire:model.defer="descricao_produto" id="descricao_produto"
+                        class="form-control border rounded py-1 px-2">
+            </div>
+            <div class="col-md-5">
+                <label class="text-dark"><strong>Código de barras</strong></label>
+                <input type="text" wire:model.defer="codigo_barras_produto" id="codigo_barras_produto"
+                        class="form-control border rounded py-1 px-2">
+            </div>
         </div>
-        <div class="col-md-1">
-            <label class="text-dark"><strong>Códido</strong></label>
-            <input type="text" placeholder="Código" wire:model.defer="id_produto" id="id"
-                    class="form-control border rounded py-1 px-2 shadow-sm">
-        </div>
-        <div class="col-md-3">
-            <label class="text-dark"><strong>Código de barras</strong></label>
-            <input type="text" placeholder="Código de barras" wire:model.defer="codigo_barras_produto" id="codigo_barras_produto"
-                    class="form-control border rounded py-1 px-2 shadow-sm">
-        </div>
-        <div class="col-md-3">
-            <label class="text-dark"><strong>Fornecedor</strong></label>
-            <input type="text" placeholder="Fornecedor" wire:model.defer="fornecedor_produto" id="fornecedor_produto"
-                    class="form-control border rounded py-1 px-2 shadow-sm">
-        </div>
-        <div class="col-md-2">
-            <label class="text-dark"><strong>Categoria</strong></label>
-            <input type="text" placeholder="Categoria" wire:model.defer="categoria_produto" id="categoria_produto"
-                    class="form-control border rounded py-1 px-2 shadow-sm">
-        </div>
-        <div class="row text-center">
-            <div class="col-md">
-                <button type="button" class="btn btn-secondary shadow-sm btn-sm mt-3 px-4" wire:click="search_produto">Pesquisar</button>
-                <a href="{{ route('clientes.pdf') }}" target="_blank" title="Imprimir lista de clientes" 
-                    class="btn btn-secondary btn-sm px-auto mt-3">
+        <div class="row">
+            <div class="col-md-3">
+                <label class="text-dark"><strong>Categoria</strong></label>
+                <input type="text" wire:model.defer="categoria_produto" id="categoria_produto"
+                        class="form-control border rounded py-1 px-2">
+            </div>
+            <div class="col-md-4">
+                <label class="text-dark"><strong>Fornecedor</strong></label>
+                <input type="text" wire:model.defer="fornecedor_produto" id="fornecedor_produto"
+                        class="form-control border rounded py-1 px-2">
+            </div>
+            <div class="col-md-2">
+                <label class="text-dark"><strong>Status</strong></label>
+                <select class="form-select border rounded py-1 px-3" wire:model.defer="status_cliente" id="status_cliente">
+                    <option value="">Todos</option>
+                    <option value=1>Ativo</option>
+                    <option value=0>Inativo</option>
+                </select>
+            </div>
+            <!-- botoes do formulario -->
+            <div class="col-md-3">
+                <button type="button" class="btn btn-primary btn-sm mt-4 px-5" wire:click="search_produto">Pesquisar</button>
+                <a href="{{ route('clientes.pdf') }}" target="_blank" title="Imprimir lista de produtos" 
+                    class="btn btn-secondary btn-sm px-auto mt-4">
                     <i class="bi bi-printer"></i>
                 </a>
-                <button type="button" class="btn btn-sm btn-secondary px-auto mt-3" onclick="location.reload();">
-                    <i class="bi bi-arrow-clockwise"></i>
+                <button type="button" class="btn btn-sm btn-secondary px-auto mt-4" onclick="location.reload();">
+                    Limpar <i class="bi bi-arrow-clockwise"></i>
                 </button>
             </div>
         </div>
     </form>
 
-    <!-- tabela de clientes -->
-    <div class="row table-responsive p-2 bg-white shadow rounded">
+    <!-- tabela de produtos -->
+    <div class="row table-responsive p-2 bg-white shadow rounded border">
         <table class="table table-hover table-striped table-bordered caption-top text-center sortable">
-            <caption>Lista de clientes</caption>
+            <caption>Lista de produtos</caption>
             <thead>
                 <tr>
                     <th scope="col">Código</th>
@@ -69,7 +80,7 @@
                         <td>{{ $produto->tipo_medida->representacao_medida }}</td>
                         <td>{{ $produto->quantidade_produto }}</td>
                         <td>{{ $produto->preco_custo_produto }}</td>
-                        <td>{{ $produto->desconto_produto ?? '0'}}</td>
+                        <td>{{ $produto->desconto_produto ?? ' - '}}</td>
                         <td>{{ $produto->preco_venda_produto }}</td>
                         @if($produto->status_produto != 0)
                             <td class="text-success">Ativo</td>
@@ -83,7 +94,7 @@
                             </button>
                         </td>
                         <td>
-                            <a class="btn btn-secondary btn-sm" title="Editar produto" href="{{ route('produtos.edit', $produto) }}">
+                            <a class="btn btn-dark btn-sm" title="Editar produto" href="{{ route('produtos.edit', $produto) }}">
                                 <i class="bi bi-pencil-square"></i>
                             </a>
                         </td>
