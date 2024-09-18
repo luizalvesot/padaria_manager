e6d063a129cf401e222880deac6e7969dccb0da6
luizalvesot <alvesluizotavio23@gmail.com>
2024-09-17 22:59:05 -0300

    implementada pagina de listagem de produtos

--- resources/views/manager/produtos/index.blade.php
+++ resources/views/manager/produtos/index.blade.php
@@ -4,19 +4,23 @@
             <div class="col-md-7">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Produtos</li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            <strong>Produtos</strong>
+                        </li>
                     </ol>
                 </nav>
             </div>
             <div class="col-md-5">
-                <a href="{{ route('categorias.show') }}" title="Categorias de produtos" class="btn btn-sm btn-light shadow-sm px-3">
+                <a href="{{ route('categorias.show') }}" title="Categorias de produtos" 
+                    class="btn btn-sm btn-secondary shadow-sm px-3">
                     Categorias de produtos
                 </a>
-                <a href="{{ route('medidas.show') }}" title="Medidas de produtos" class="btn btn-sm shadow-sm btn-light px-3">
+                <a href="{{ route('medidas.show') }}" title="Medidas de produtos" 
+                    class="btn btn-sm shadow-sm btn-secondary px-3">
                     Tipos de medidas
                 </a>
-                <a href="{{-- route('produtos.create') --}}" title="Cadastrar produto" class="btn btn-sm btn-success px-3">
+                <a href="{{ route('produtos.create') }}" title="Cadastrar produto" 
+                    class="btn btn-sm btn-success px-3">
                     Cadastrar produtos
                     <i class="bi bi-plus-lg"></i>
                 </a>
