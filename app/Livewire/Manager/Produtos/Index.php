18a0ecefbeb56b5ae86884c72c1fde1b8889c128
luizalvesot <alvesluizotavio23@gmail.com>
2024-09-21 15:16:57 -0300

    implementado pesquisa e mostragem de produtos

--- app/Livewire/Manager/Produtos/Index.php
+++ app/Livewire/Manager/Produtos/Index.php
@@ -11,37 +11,58 @@ class Index extends Component
     use WithPagination;
 
     public $descricao_produto = '';
-    public $id_produto = null;
+    public $id_produto;
     public $codigo_barras_produto = '';
-    public $fornecedor_produto = '';
-    public $categoria_produto = '';
+    public $fornecedor_produto;
+    public $categoria_produto;
+    public $status;
+    public $paginate = 15;
+
+    public $fornecedores;
+    public $categoria_produtos;
 
     public function search_produto()
     {
         $this->resetPage();
     }
 
+    public function mount($fornecedores, $categoria_produtos)
+    {
+        $this->fornecedores = $fornecedores;
+        $this->categoria_produtos = $categoria_produtos;
+    }
+
     public function render()
     {
-        if($this->id_produto != null)
-        {
-            $produtos = Produto::where('descricao_produto', 'like', "%".$this->descricao_produto."%")
-                    ->where('id', $this->id_produto)
-                    ->where('codigo_barras_produto', 'like', "%".$this->codigo_barras_produto."%")
-                    ->where('fornecedor', 'like', "%".$this->fornecedor_produto."%")
-                    ->where('categoria_produto', 'like', "%".$this->categoria_produto."%")
-                    ->paginate(15);
+        $query = Produto::query();
 
-            return view('livewire.manager.produtos.index', compact('produtos'));
-        } else {
-            $produtos = Produto::where('descricao_produto', 'like', "%".$this->descricao_produto."%")
-                    ->where('codigo_barras_produto', 'like', "%".$this->codigo_barras_produto."%")
-                    ->where('fornecedor', 'like', "%".$this->fornecedor_produto."%")
-                    ->where('categoria_produto', 'like', "%".$this->categoria_produto."%")
-                    ->paginate(15);
-            
-            return view('livewire.manager.produtos.index', compact('produtos'));
+        if($this->descricao_produto){
+            $query->where('descricao_produto', 'like', '%'.$this->descricao_produto.'%');
         }
+
+        if($this->id_produto){
+            $query->find($this->id_produto);
+        }
+
+        if($this->codigo_barras_produto){
+            $query->where('codigo_barras_produto', 'like', '%'.$this->codigo_barras_produto.'%');
+        }
+
+        if($this->fornecedor_produto){
+            $query->where('fornecedor', $this->fornecedor_produto);
+        }
+
+        if($this->categoria_produto){
+            $query->where('categoria_produto', $this->categoria_produto);
+        }
+
+        if($this->status){
+            $query->where('status_produto', $this->status);
+        }
+
+        $produtos = $query->get();
+
+        return view('livewire.manager.produtos.index', compact('produtos'));
     }
 
     public function updatingSearch()
