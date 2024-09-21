bbdc86979a7ebd34a244c1b333994e0c2372b80f
luizalvesot <alvesluizotavio23@gmail.com>
2024-09-21 15:12:36 -0300

    tela para editar produtos implementada

---
+++ resources/views/manager/produtos/edit.blade.php
@@ -0,0 +1,29 @@
+<x-app-layout>
+    <div class="container">
+        <div class="row pt-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('produtos.show') }}">produtos</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Editar</li>
+                </ol>
+            </nav>
+        </div><hr>
+        
+        <form method="POST" action="{{ route('produtos.update', $produto) }}">
+            @csrf
+            <div class="row bg-light p-4 shadow-lg mt-2 mb-4 rounded">
+                @method('PUT')
+                @include('manager.produtos._form-fields')
+
+                <div class="row text-right mt-4">
+                    <div class="col">
+                        <a href="{{ route('produtos.show') }}" class="btn btn-secondary btn-sm px-4">Voltar</a>
+                        <button type="submit" class="btn btn-primary btn-sm active px-5">Editar</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+    </div>
+</x-app-layout>
