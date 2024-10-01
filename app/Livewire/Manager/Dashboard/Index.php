44ac6b28a4f407e08eacdbef96eecd28a8314079
luizalvesot <alvesluizotavio23@gmail.com>
2024-09-30 21:46:29 -0300

    dashboard ajustado

--- app/Livewire/Manager/Dashboard/Index.php
+++ app/Livewire/Manager/Dashboard/Index.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Manager\Dashboard;
 
 use App\Models\Manager\Clientes\Cliente;
+use App\Models\Manager\Produtos\Produto;
 
 use Livewire\Component;
 
@@ -10,11 +11,15 @@ class Index extends Component
 {
     public $clientes_ativos;
     public $clientes_inativos;
+    public $produtos_ativos;
+    public $produtos_inativos;
 
     public function mount()
     {
         $this->clientes_ativos = Cliente::where('status_cliente', 1)->count();
         $this->clientes_inativos = Cliente::where('status_cliente', 0)->count();
+        $this->produtos_ativos = Produto::where('status_produto', 'ativo')->count();
+        $this->produtos_inativos = Produto::where('status_produto', 'inativo')->count();
     }
 
     public function render()
--- resources/views/livewire/manager/dashboard/index.blade.php
+++ resources/views/livewire/manager/dashboard/index.blade.php
@@ -1,28 +1,39 @@
 <div>
     <div class="container p-4">
-        <a href="{{ route('categorias.show') }}" class="btn btn-warning px-3">Categorias de produtos</a>
         <div class="row">
             <div class="col-md-4">
-                <div class="card card-dashboard bg-secondary border shadow pb-2 mx-2 my-1">
+                <div class="card card-dashboard bg-white border rounded shadow mx-2 my-2">
                     <div class="card-body text-center">
-                        <h4 class="card-title h4 text-white">Clientes ativos</h4>
-                        <h1 class="h1 text-white" style="font-size: 62px">{{ $clientes_ativos }}</h1>
+                        <a href="{{route('clientes.show')}}">
+                            <strong class="card-title h5 text-primary">
+                                Clientes ativos <i class="bi bi-person-check-fill text-secondary"></i>
+                            </strong>
+                            <h1 class="h1 text-primary">{{ $clientes_ativos }}</h1>
+                        </a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card card-dashboard border shadow pb-2 mx-2 my-1">
+                <div class="card card-dashboard bg-white border rounded shadow mx-2 my-2">
                     <div class="card-body text-center">
-                        <h4 class="card-title h4 text-danger">Clientes inativos</h4>
-                        <h1 class="h1 text-danger" style="font-size: 62px">{{ $clientes_inativos }}</h1>
+                        <a href="{{route('clientes.show')}}">
+                            <strong class="card-title h5 text-danger">
+                                Clientes inativos <i class="bi bi-person-fill-slash text-secondary"></i>
+                            </strong>
+                            <h1 class="h1 text-danger">{{ $clientes_inativos }}</h1>
+                        </a>    
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card card-dashboard border shadow bg-success pb-2 mx-2 my-1">
+                <div class="card card-dashboard bg-white border rounded shadow mx-2 my-2">
                     <div class="card-body text-center">
-                        <h4 class="card-title h4 text-white">Produtos ativos</h4>
-                        <h1 class="h1 text-white" style="font-size: 62px">0</h1>
+                        <a href="{{route('produtos.show')}}">
+                            <strong class="card-title h5 text-primary">
+                                Produtos ativos <i class="bi bi-box-fill"></i>
+                            </strong>
+                            <h1 class="h1 text-primary">{{ $produtos_ativos }}</h1>
+                        </a>    
                     </div>
                 </div>
             </div>
@@ -30,26 +41,38 @@
         <!-- segunda linha -->
         <div class="row">
             <div class="col-md-4">
-                <div class="card card-dashboard bg-danger border shadow pb-2 mx-2 my-1">
+                <div class="card card-dashboard bg-white border rounded shadow mx-2 my-2">
                     <div class="card-body text-center">
-                        <h4 class="card-title h4 text-white">Produtos inativos</h4>
-                        <h1 class="h1 text-white" style="font-size: 62px">0</h1>
+                        <a href="{{route('produtos.show')}}">
+                            <strong class="card-title h5 text-danger">
+                                Produtos inativos <i class="bi bi-box-fill"></i>
+                            </strong>
+                            <h1 class="h1 text-danger">{{ $produtos_inativos }}</h1>
+                        </a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card card-dashboard border shadow pb-2 mx-2 my-1">
+                <div class="card card-dashboard bg-success border rounded shadow mx-2 my-2">
                     <div class="card-body text-center">
-                        <h4 class="card-title h4"></h4>
-                        <h1 class="h1 text-secondary" style="font-size: 62px"> + </h1>
+                        <a href="{{route('vendas.show')}}" title="acessar painel de vendas">
+                            <strong class="card-title h5 text-white">
+                                Vendas finalizadas
+                            </strong>
+                            <h1 class="h1 text-white">2</h1>
+                        </a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card card-dashboard border shadow pb-2 mx-2 my-1">
+                <div class="card card-dashboard bg-danger border rounded shadow mx-2 my-2">
                     <div class="card-body text-center">
-                        <h4 class="card-title h4"></h4>
-                        <h1 class="h1 text-secondary" style="font-size: 62px"> + </h1>
+                        <a href="{{route('vendas.show')}}" title="acessar painel de vendas">
+                            <strong class="card-title h5 text-white">
+                                Vendas abertas
+                            </strong>
+                            <h1 class="h1 text-white">5</h1>
+                        </a>
                     </div>
                 </div>
             </div>
@@ -57,10 +80,12 @@
         <!-- terceira linha -->
         <div class="row">
             <div class="col-md-4">
-                <div class="card card-dashboard border shadow pb-2 mx-2 my-1">
+                <div class="card card-dashboard bg-white border rounded shadow mx-2 my-2">
                     <div class="card-body text-center">
-                        <h4 class="card-title h4"></h4>
-                        <h1 class="h1 text-secondary" style="font-size: 62px"> + </h1>
+                        <a href="{{route('clientes.show')}}" title="acessar menu de clientes">
+                            <strong class="card-title h5 text-dark">Clientes negativos</strong>
+                            <h1 class="h1 text-dark">1</h1>
+                        </a>    
                     </div>
                 </div>
             </div>
