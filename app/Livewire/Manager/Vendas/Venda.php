8c78a4879e7865e1659c17131e4985fc1eee7908
luizalvesot <alvesluizotavio23@gmail.com>
2024-10-09 21:36:21 -0300

    escrito comentários para organizar as funções

--- app/Livewire/Manager/Vendas/Venda.php
+++ app/Livewire/Manager/Vendas/Venda.php
@@ -15,62 +15,85 @@ class Venda extends Component
     public $formasPagamento;
     public $selectedCliente;
     public $selectedFormaPagamento;
-    public $carrinho = [];
-    public $total = 0;
-    public $searchTerm = ''; // Variável para armazenar o termo de pesquisa
-    public $barcode = ''; // Variável para armazenar o código de barras
-    public $valorPago = 0; // Valor pago pelo cliente
-    public $troco = 0; // Troco a ser calculado
+    public $carrinho = [];         // Array que armazena os produtos da compra
+    public $total = 0;             // Inicializaçãoo do valor total
+    public $searchTerm = '';       // Variável para armazenar o termo de pesquisa
+    public $barcode = '';          // Variável para armazenar o código de barras
+    public $valorPago = 0;         // Variável que recebe o valor pago pelo cliente
+    public $troco = 0;             // Troco a ser calculado
 
     public function mount()
     {
-        $this->clientes = Cliente::all();
-        $this->formasPagamento = FormasPagamento::all();
-        $this->updateProdutos(); // Carrega os produtos inicialmente
+        $this->clientes = Cliente::all();                // Recebe todos os clientes cadastrados
+        $this->formasPagamento = FormasPagamento::all(); // Recebe todas as formas de pagamento cadastrados
+        $this->updateProdutos();                         // Chama o método para carregar todos os produtos inicialmente
     }
 
-    // Atualiza a lista de produtos conforme o termo de pesquisa
+    /**
+     * Método que atualiza a lista de produtos conforme o termo de pesquisa
+     * - os produtos são pesquisados apenas pela descrição e pelo id
+     * - são buscados apenas os ativos
+     */
     public function updateProdutos()
     {
         $this->produtos = Produto::where('status_produto', 'ativo')
-            ->where('descricao_produto', 'like', '%' . $this->searchTerm . '%')
-            ->orWhere('id', 'like', '%' . $this->searchTerm . '%')
-            ->get();
+             ->where('descricao_produto', 'like', '%' . $this->searchTerm . '%')
+             ->orWhere('id', 'like', '%' . $this->searchTerm . '%')
+             ->get();
     }
 
-    // Função para adicionar produto pelo código de barras
+    /**
+     * Método responsável por procurar o produto pelo código de barras e adicionar no carrinho
+     */
     public function updatedBarcode()
     {
+        // Localiza o produto no banco de dados de acordo com o código de barras
         $produto = Produto::where('codigo_barras_produto', $this->barcode)
                             ->where('status_produto', 'ativo')
                             ->first();
         
         if ($produto) {
-            $this->adicionarProduto($produto->id); // Adiciona o produto ao carrinho
+            $this->adicionarProduto($produto->id); // Chama método para adicionar o produto ao carrinho
             $this->barcode = ''; // Limpa o campo de código de barras após adicionar o produto
         } else {
             session()->flash('message', 'Produto não encontrado.');
         }
     }
 
+    /**
+     * Método responsável por filtrar os produtos sempre que o searchTerm for atualizado
+     */
     public function updatedSearchTerm()
     {
-        // Sempre que o searchTerm for atualizado, os produtos serão filtrados
         $this->updateProdutos();
     }
 
+    /**
+     * Método que adiciona os produtos no carrinho
+     * - recebe por parâmetro o ID do produto e o localiza no banco de dados
+     * - insere o objeto produto, a quantidade, o preco dele e a data atual no array carrinho
+     * - chama o método para atualizar o valor total do carrinho
+     */
     public function adicionarProduto($produtoId)
     {
         $produto = Produto::find($produtoId);
+        
         $this->carrinho[] = [
-            'produto' => $produto,
-            'quantidade' => 1,
-            'preco' => $produto->preco_venda_produto,
-            'adicionado_em' => Carbon::now() // Armazena a data e hora atuais
+            'produto'       => $produto,
+            'quantidade'    => 1,
+            'preco'         => $produto->preco_venda_produto,
+            'adicionado_em' => Carbon::now()
         ];
+
         $this->atualizarTotal();
     }
 
+    /**
+     * Método reponsável por atualizar a quantidade de cada produto no carrinho, quando necessário
+     * - recebe o index do produto no carrinho e a nova quantidade no parâmetro e faz a substituição no array
+     * - chama o método para atualizar o valor total do carrinho
+     * - se a nova quantidade do produto for igual ou menor a zero, chma-se o método para remover produto do array
+     */
     public function atualizarQuantidade($index, $novaQuantidade)
     {
         if ($novaQuantidade > 0) {
@@ -81,6 +104,12 @@ class Venda extends Component
         }
     }
 
+    /**
+     * Método utilizado para remover produto do carrinho
+     * - a função UNSET do PHP é usada para destruir um ídice do array, nesse caso remove o produto do carrinho
+     * - é chamado a função ARRAY_VALUES do PHP para reorganizar os índices do array
+     * - o método para atualizar o valor total é invocado
+     */
     public function removerProduto($index)
     {
         unset($this->carrinho[$index]);
@@ -88,22 +117,31 @@ class Venda extends Component
         $this->atualizarTotal();
     }
 
+    /**
+     * Método utilizado para atualizar o valor total do carrinho
+     * - a função ARRAY_REDUCE do PHP pega o preço de cada item do carrinho e multiplica pela sua quantidade para retornar o valor total do carrinho
+     * - invoca a função para calcular o troco a devolver para o cliente
+     */
     public function atualizarTotal()
     {
         $this->total = array_reduce($this->carrinho, function ($total, $item) {
             return $total + ($item['quantidade'] * $item['preco']);
         }, 0);
 
-        // Sempre recalcula o troco ao atualizar o total
-        $this->calcularTroco();
+        $this->calcularTroco(); // Sempre recalcula o troco ao atualizar o total
     }
 
+    /**
+     * Método utiliza para chamar o cálculo de troco quando o valor pago é atualizado
+     */
     public function updatedValorPago($value)
     {
-        // Quando o valor pago é atualizado, recalcula o troco
         $this->calcularTroco();
     }
 
+    /**
+     * Função utilizada para calcular o troco do cliente de acordo com o valor pago
+     */
     public function calcularTroco()
     {
         if ($this->valorPago >= $this->total) {
@@ -113,14 +151,20 @@ class Venda extends Component
         }
     }
 
+    /**
+     * Método utilizado para salvar a venda no banco de dados
+     */
     public function salvarVenda()
     {
-        // Lógica para registrar a venda no banco de dados
+        
     }
 
+    /**
+     * Método utilizado para finalizar a venda, atualizando as informações no banco de dados
+     */
     public function finalizarVenda()
     {
-        // Lógica para registrar a venda no banco de dados
+        
     }
 
     public function render()
