3545b1532b58199c98492f9d2598ae3add2571ee
luizalvesot <alvesluizotavio23@gmail.com>
2024-10-14 23:10:26 -0300

    criacao de tabela auxiliar de vendas

---
+++ database/migrations/2024_10_15_020622_create_aux_vendas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aux_vendas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('venda')
+                    ->references('id')
+                    ->on('vendas')
+                    ->onDelete('restrict');
+            $table->foreignId('produto')
+                    ->references('id')
+                    ->on('produtos')
+                    ->onDelete('restrict');
+            $table->foreignId('cliente')
+                    ->references('id')
+                    ->on('clientes')
+                    ->onDelete('restrict');
+            $table->integer('qtd_produto');
+            $table->dateTime('horario_venda');
+            $table->string('tipo_venda', 30);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aux_vendas');
+    }
+};
