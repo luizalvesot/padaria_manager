0401e4c45f256764640716cacef3214c6fcd2eb2
luizalvesot <alvesluizotavio23@gmail.com>
2025-02-10 00:23:21 -0300

    ajuste no navbar para facilitar a interação do usuário

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,15 +13,15 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Home') }}
                     </x-nav-link>
 
-                    <x-nav-link href="{{ route('listagem.show') }}" :active="request()->routeIs('listagem.show')">
-                        {{ __('Listagem de vendas') }}
-                    </x-nav-link>
-
-                    <x-nav-link href="{{ route('vendas.show') }}" :active="request()->routeIs('vendas.show')">
+                    <x-nav-link class="text-primary" href="{{ route('vendas.show') }}" :active="request()->routeIs('vendas.show')">
                         {{ __('Vendas') }}
+                    </x-nav-link>
+
+                    <x-nav-link class="text-primary" href="{{ route('listagem.show') }}" :active="request()->routeIs('listagem.show')">
+                        {{ __('Listagem de vendas') }}
                     </x-nav-link>
 
                     <x-nav-link href="{{ route('produtos.show') }}" 
